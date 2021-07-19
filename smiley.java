f8e7845e6d9f821d4133ad5669ff69125992fd59
camilamc02 <85080169+camilamc02@users.noreply.github.com>
2021-07-18 23:41:29 -0400

    Add files via upload

---
+++ smiley.java
@@ -0,0 +1,13 @@
+public class smiley {
+	public static void main(String[] args)
+	{
+		System.out.println("  xxxx  ");
+		System.out.println(" x    x ");
+		System.out.println("x x  x x");
+		System.out.println("x      x");
+		System.out.println("x x  x x");
+		System.out.println("x  xx  x");
+		System.out.println(" x    x ");
+		System.out.println("  xxxx  ");
+	}
+}
